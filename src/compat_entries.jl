3ef49d9a5cbdd220d07e4452467160401c7207c1
Dilum Aluthge <dilum@aluthge.com>
2019-11-11 04:01:16 -0500

    Attempt to preserve lower bounds, when possible (#4)

--- src/compat_entries.jl
+++ src/compat_entries.jl
@@ -1,18 +1,49 @@
 import Pkg
 import UUIDs
 
-function generate_compat_entry(latest_dep_version::VersionNumber)::String
-    major = latest_dep_version.major
-    minor = latest_dep_version.minor
-    patch = latest_dep_version.patch
-    compat = "0 - $(major).$(minor).$(patch)"
+function new_compat_entry(current_compat_entry::Vector,
+                          latest_dep_version::VersionNumber)::String
+    return new_compat_entry(latest_dep_version)
+end
+
+function new_compat_entry(current_compat_entry::String,
+                          latest_dep_version::VersionNumber)::String
+    myregex_1 = r"([\d]*)\.([\d]*)\.([\d]*)[\s]*-[\s]*\*"
+    if occursin(myregex_1, current_compat_entry)
+        m = match(myregex_1, current_compat_entry)
+        lower_bound = VersionNumber("$(m[1]).$(m[2]).$(m[3])")
+        return new_compat_entry(lower_bound, latest_dep_version)
+    else
+        return new_compat_entry(latest_dep_version)
+    end
+end
+
+function new_compat_entry(latest_dep_version::VersionNumber)::String
+    lower_bound = v"0"
+    return new_compat_entry(lower_bound, latest_dep_version)
+end
+
+function new_compat_entry(lower_bound::VersionNumber,
+                          latest_dep_version::VersionNumber)::String
+    a = lower_bound.major
+    b = lower_bound.minor
+    c = lower_bound.patch
+    
+    x = latest_dep_version.major
+    y = latest_dep_version.minor
+    z = latest_dep_version.patch
+    
+    compat = "$(a).$(b).$(c) - $(x).$(y).$(z)"
+    
     @assert Pkg.Types.VersionRange(compat) isa Pkg.Types.VersionRange
     return compat
 end
 
-function generate_compat_entry(strategy::CapStrategy, current_compat_entry, latest_dep_version::VersionNumber)
+function generate_compat_entry(strategy::CapStrategy,
+                               current_compat_entry::Union{Vector, String},
+                               latest_dep_version::VersionNumber)
     if length(current_compat_entry) == 0
-        return generate_compat_entry(latest_dep_version)
+        return new_compat_entry(current_compat_entry, latest_dep_version)
     else
         spec = _entry_to_spec(current_compat_entry)
         return generate_compat_entry(strategy, current_compat_entry, spec, latest_dep_version)
@@ -39,22 +70,28 @@ function _entry_to_spec(current_compat_entry::Vector{String})::Pkg.Types.Version
     spec = Pkg.Types.VersionSpec(ranges)
 end
 
-function generate_compat_entry(::NoCompatEntry, current_compat_entry, spec::Pkg.Types.VersionSpec, latest_dep_version::VersionNumber)
+function generate_compat_entry(::NoCompatEntry,
+                               current_compat_entry::Union{Vector, String},
+                               spec::Pkg.Types.VersionSpec,
+                               latest_dep_version::VersionNumber)
     if length(current_compat_entry) == 0
-        return generate_compat_entry(latest_dep_version)
+        return new_compat_entry(current_compat_entry, latest_dep_version)
     else
         return current_compat_entry
     end
 end
 
-function generate_compat_entry(::NoUpperBound, current_compat_entry, spec::Pkg.Types.VersionSpec, latest_dep_version::VersionNumber)
+function generate_compat_entry(::NoUpperBound,
+                               current_compat_entry::Union{Vector, String},
+                               spec::Pkg.Types.VersionSpec,
+                               latest_dep_version::VersionNumber)
     if length(current_compat_entry) == 0
-        return generate_compat_entry(latest_dep_version)
+        return new_compat_entry(current_compat_entry, latest_dep_version)
     else
         if has_upper_bound(spec)
             return current_compat_entry
         else
-            return generate_compat_entry(latest_dep_version)
+            return new_compat_entry(current_compat_entry, latest_dep_version)
         end
     end
 end
